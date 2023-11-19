f6d97f65d90bba91587bdeb7ed273bbf22e4fa3d
suletha <suletha.gopi@gmail.com>
2023-11-19 15:44:36 +0530

    c1

---
+++ routes/contact.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/contactus', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contactus.html'));
+});
+  module.exports = router;
