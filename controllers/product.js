4f35e8f9af263c8eedb9219346382817ef94d244
suletha <suletha.gopi@gmail.com>
2023-11-21 14:00:49 +0530

    c1

---
+++ controllers/product.js
@@ -0,0 +1,9 @@
+export.getAddProduct = (req,res,next) =>{
+    res.render('add-product',{
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
+};
